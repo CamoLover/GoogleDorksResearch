903c4e2723f9eda4914f67db4bbdb9b1ca494ca7
Evan E <evan.escabasse@gmail.com>
2023-03-10 21:21:18 +0100

    Background and optimisation
    optimised the code a little
    added a background gradient for more ~style~

--- GoogleDorksResearch.py
+++ GoogleDorksResearch.py
@@ -1,42 +1,56 @@
 import webbrowser
 from  tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 window=Tk()
-
 
 def dorks(name, site):
     dorks = ["intitle","intext","inurl"]#dorks list
-    linknumber = 0 #current number in dorks array
     name_accurate = '"'+name+'"' #Remake name in correct format
     if site == "" : #if no website to search
         for i in range(len(dorks)) :
-            webbrowser.open("https://www.google.fr/search?q="+dorks[linknumber]+":"+name_accurate) #search name with dorks
-            linknumber+=1
+            webbrowser.open("https://www.google.fr/search?q="+dorks[i]+":"+name_accurate) #search name with dorks
     else : #if there is a specific website
         for i in range(len(dorks)) :
-            webbrowser.open("https://www.google.fr/search?q="+dorks[linknumber]+":"+name_accurate+" site:"+site) #search name with dorks and on specific website
-            linknumber+=1 
+            webbrowser.open("https://www.google.fr/search?q="+dorks[i]+":"+name_accurate+" site:"+site) #search name with dorks and on specific website
         webbrowser.open("https://www.google.fr/search?q="+name_accurate+" site:"+site) #search name without dorks, on specific website
 
 #----------------------------------------------------------
 #tkinter part
 
+class Example(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+        self.image = Image.open("background.png")
+        self.img_copy= self.image.copy()
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+    def _resize_image(self,event):
+        new_width = event.width
+        new_height = event.height
+        self.image = self.img_copy.resize((new_width, new_height))
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+e = Example(window)
+e.pack(fill=BOTH, expand=YES)
 
 lbl=Label(window, text="Google Dorks Research")
-lbl.place(x=80, y=50)
+lbl.place(relx=.5, rely=.05,anchor= CENTER)
 lbl=Label(window, text="Name :")
-lbl.place(x=20, y=100)
-txtfldname=Entry(window, text="Name", bd=5)
-txtfldname.place(x=80, y=100)
+lbl.place(relx=.2, rely=.3,anchor= CENTER)
+txtfldname=Entry(window, bd=5)
+txtfldname.place(relx=.5, rely=.3,anchor= CENTER)
 lbl=Label(window, text="Website :")
-lbl.place(x=20, y=125)
-txtfldsite=Entry(window, text="Website", bd=5)
-txtfldsite.place(x=80, y=125)
+lbl.place(relx=.2, rely=.4,anchor= CENTER)
+txtfldsite=Entry(window, bd=5)
+txtfldsite.place(relx=.5, rely=.4,anchor= CENTER)
 btn=Button(window, text="research", command=lambda : dorks(txtfldname.get(), txtfldsite.get()))
-btn.place(x=120, y=175)
+btn.place(relx=.5, rely=.5,anchor= CENTER)
 
 
-
+window.tk.call('wm', 'iconphoto', window._w, ImageTk.PhotoImage(file='logo.png'))
 window.title('Google dorks research')
 window.geometry("300x300")
 window.mainloop()
